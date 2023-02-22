ecce61ea306c0f738f2dba85a779435a300632db
Alibek <alibek.jamilov@gmail.com>
2023-02-22 14:07:01 +0500

    Add welcome for lovely user

---
+++ Ex5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Write name of user: "); 
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Welcome, Mashaxon!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+
+}
